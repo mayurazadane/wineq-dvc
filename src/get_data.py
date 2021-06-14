9be1184ba691c335b4767c30930eb50de6f8ad09
Mayura Zadane <mayuzadane19@gmail.com>
2021-06-14 17:02:51 +0530

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -1,7 +1,7 @@
 # read params
 # process
 # return dataframe
-import os
+# import os
 import yaml
 import pandas as pd
 import argparse
@@ -12,16 +12,18 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
+
 def get_data(config_path):
     config = read_params(config_path)
     # print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
+    # print(df.head())
     return df
 
-
-if __name__=="__main__":
+# extra comment
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default="params.yaml")
+    args.add_argument("--config", default="D:/Mayura/MY ML COMPLETE NOTES/winq/params.yaml")
     parsed_args = args.parse_args()
     data = get_data(config_path=parsed_args.config)
---
+++ src/load_data.py
@@ -0,0 +1,19 @@
+# read data from data source
+# save itin the data/raw for further process
+import os
+import argparse
+from get_data import read_params, get_data
+
+def load_and_save(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    new_cols = [col.replace(" ", "_") for col in df.columns]
+    raw_data_path = config['load_data']['raw_dataset_csv']
+    df.to_csv(raw_data_path, sep=",", index=False, header=new_cols)
+    # print(new_cols)
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="D:/Mayura/MY ML COMPLETE NOTES/winq/params.yaml")
+    parsed_args = args.parse_args()
+    data = load_and_save(config_path=parsed_args.config)
