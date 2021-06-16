f47428679621467eb30db610ba162e61c011bb5b
Mayura Zadane <mayuzadane19@gmail.com>
2021-06-16 11:09:21 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description = "Its a wine q package",
+    author="mayura",
+    packages=find_packages(),
+    license="MIT"
+)
